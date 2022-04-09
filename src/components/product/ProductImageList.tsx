8df33410ba307326330e47a4aeb515e938a2f1ef
peterfulop <mrpeterfulop@gmail.com>
2022-04-09 14:16:13 +0200

    refact(*): refact components hrefs;

--- src/components/product/ProductImageList.tsx
+++
@@ -1,9 +0,0 @@
-export default function ProductImageList(props: { images?: string[] }) {
-  return (
-    <div className="product-image-list">
-      {props.images?.map((image) => (
-        <img src={image} alt={image} key={image} />
-      ))}
-    </div>
-  );
-}
