f29b0026cc29f8a3933091f535f9be4609ca6f0f
peterfulop <mrpeterfulop@gmail.com>
2022-04-14 20:50:30 +0200

    fix(src): src folder removed;

--- src/components/UI/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-import "../../Styles/UI/LoadingSpinner.css";
-
-const LoadingSpinner = () => {
-  return <div className="spinner"></div>;
-};
-
-export default LoadingSpinner;
--- src/pages/NotFoundPage.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFoundPage() {
-  return (
-    <section className="d-flex justify-content-center m-5">
-      <h2>404</h2>
-      <p className="ms-2">Page Not Found</p>
-    </section>
-  );
-}
