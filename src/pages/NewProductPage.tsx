6414d53dc5b35c3e5f488d75493c20a468205afa
peterfulop <mrpeterfulop@gmail.com>
2022-04-19 12:56:31 +0200

    refact(*): new product page updated;

--- src/pages/NewProductPage.tsx
+++ src/pages/NewProductPage.tsx
@@ -1,17 +1,14 @@
 import { Fragment, useContext } from "react";
 import AddNewProduct from "../components/products/AddNewProduct";
 import { ProductContext } from "../contexts/product.context";
-import useHttp from "../hooks/use-http";
 import { addProduct, getAllProducts } from "../lib/api";
 import { NewProduct } from "../utils/interfaces/product.interface";
 
 export default function NewProductPage() {
-  const { sendRequest } = useHttp(addProduct);
-
   const ctx = useContext(ProductContext);
 
   const addNewProductHandler = async (productData: NewProduct) => {
-    await sendRequest(productData);
+    await addProduct(productData);
     const products = await getAllProducts();
     ctx.setItems(products);
   };
